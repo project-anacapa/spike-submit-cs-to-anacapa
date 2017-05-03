11f5d4d38ff22a126400853920634050708271a5
Carson Holoien <chilloien@gmail.com>
2017-05-03 14:36:57 -0700

    CH/PC Add flag to allow switching between github.com and github.ucsb.edu

--- spike1.rb
+++ spike1.rb
@@ -10,8 +10,8 @@ options[:count]=1
 
 OptionParser.new do |parser|
 
-  parser.on("-c", "--count COUNT", Integer, "Repeat the message COUNT times") do |v|
-    options[:count] = v
+  parser.on("-c", "--com", "Use github.com instead of github.ucsb.edu") do |v|
+    options[:com] = v
   end
   
 end.parse!
@@ -19,9 +19,10 @@ end.parse!
 # This items should be the same for all students
 
 puts "Hello Spike"
-
-Octokit.configure do |c|
-  c.api_endpoint = "https://github.ucsb.edu/api/v3/"
+if !options[:com]
+	Octokit.configure do |c|
+	  c.api_endpoint = "https://github.ucsb.edu/api/v3/"
+	end
 end
 
 client = Octokit::Client.new(:access_token => ENV["GITHUB_PERSONAL_ACCESS_TOKEN"])
