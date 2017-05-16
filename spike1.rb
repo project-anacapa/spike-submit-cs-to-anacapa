29a42d1c66289b317273ce3bd72c9a37af369c47
Carson Holoien <chilloien@gmail.com>
2017-05-15 22:46:44 -0700

    Write script to check is a member of an organization. Write script to check if repositories are already in the organization.  If they are not already added then create it.

--- spike1.rb
+++ spike1.rb
@@ -2,9 +2,88 @@
 
 require 'optparse'
 require 'octokit'
+include Octokit
+
+require 'json'
+
+
+
+# https://github.com/project-anacapa/course-github-org-tool/blob/feature/assignment-view/app/jobs/checkout_assignment_job.rb
+# https://github.com/project-anacapa/course-github-org-tool/blob/development/app/controllers/assignments_controller.rb
+
+
 
 # This will hold the options we parse
 options = {}
+
+
+course_json = %{
+	{
+		"org_name" : "clholoien-testorg-1",
+		"projects":[
+			{"name" : "lab00"},
+			{"name" : "lab01"},
+			{"name" : "lab02"}
+		]
+	}
+}
+
+
+def process_course(course_json, client)
+
+	user = client.user
+	course_info = JSON.parse(course_json)
+
+	# See whether course_info["org_name"] is an organization that user has access to.
+	org_name = course_info["org_name"]
+
+	user_in_org = true
+
+	# Check if user has access to this organization.
+	if client.organization_member?(org_name, user.login)
+		puts "User is part of the organization - " + org_name
+	else
+		puts "User is NOT part of the organization - " + org_name
+	end
+
+	for project in course_info["projects"]
+
+		proj_name = project["name"]
+		proj_repo_fullname =  "#{org_name}/#{proj_name}"
+
+		existed  = true
+
+		begin 
+			proj_repo = client.repo(proj_repo_fullname)
+		rescue
+			existed = false
+		end
+
+		if ! existed	
+			client.create_repository( proj_name , {  
+				:organization => org_name,
+				:private => true # change this to true when we know that private repos are feasible.
+			} )			
+			puts "Created repo " + project["name"]
+		else
+			puts project["name"] + " already exists."
+		end
+
+	end
+
+	# For each of the projects, determine if there already exists a project with this particular name.
+
+	# e.g. lab00 or lab01 => 
+	# if doesn't exist create it.
+	# For each of those repos see if there is a folder .anacapa/assignment_spec.json
+	
+end
+
+
+# def create_project(user, project_name)
+
+# end
+
 
 options[:count]=1
 
@@ -31,6 +110,8 @@ else
 	exit
 end
 
-client = Octokit::Client.new(:access_token => token)
-user = client.user
-puts user.login
+client = Octokit::Client.new(:access_token => token, 
+							:api_endpoint => "https://github.ucsb.edu/api/v3/"
+							)
+process_course(course_json, client)
+
